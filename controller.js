0c20fa81dc386935479781f02fc1a415171c8340
iqbal andiansyah <almunsi.soft@gmail.com>
2021-02-28 11:29:52 +0700

    menampilan data berdasarkan id

--- controller.js
+++ controller.js
@@ -11,9 +11,23 @@ exports.index = function(req, res){
 exports.tampilsemuamahasiswa = function(req, res){
     connection.query('SELECT * FROM mahasiswa ORDER BY id_mahasiswa ASC ', function(err, rows, fields){
         if(err){
-            connection.log(err);
+            console.log(err);
         } else {
             response.ok(rows.rows, res);
         }
     })
+}
+
+//menampilkan semua data mahasiswa berdasarkan id
+exports.tampilkanberdasarkanid = function(req, res){
+    let { id } = req.params;
+    connection.query('SELECT * FROM mahasiswa where id_mahasiswa = $1', [id], 
+        function(err, rows, fields){
+            if(err){
+                console.log(err);
+            } else {
+                response.ok(rows.rows, res);
+            }
+        }
+    );
 }
