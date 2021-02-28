91eed3754779c3f4561429475e02c4d5d9cd9895
iqbal andiansyah <almunsi.soft@gmail.com>
2021-02-28 11:05:23 +0700

    berhasil menampilkan semua data

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi REST API ku berjalan", res)
 };
+
+//menampilka semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req, res){
+    connection.query('SELECT * FROM mahasiswa ORDER BY id_mahasiswa ASC ', function(err, rows, fields){
+        if(err){
+            connection.log(err);
+        } else {
+            response.ok(rows.rows, res);
+        }
+    })
+}
